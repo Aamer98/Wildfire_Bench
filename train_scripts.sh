a7ef930257682026ab30cd93653654f643e92c67
aamer98 <ar.aamer@gmail.com>
2024-07-05 15:53:54 +0000

    configs

--- train_scripts.sh
+++ train_scripts.sh
@@ -1,8 +1,8 @@
 # wildfire unit test
-python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.strategy=ddp --trainer.devices=1 --trainer.max_epochs=50 --data.root_dir=/home/as26840@ens.ad.etsmtl.ca/data/wildfirespreadTS_unitTest --data.predict_range=24 --data.batch_size=2
+python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.devices=1 --data.root_dir=/home/as26840@ens.ad.etsmtl.ca/data/wildfirespreadTS_unitTest 
 
 # wildfire
-python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.strategy=ddp --trainer.devices=1 --trainer.max_epochs=100 --data.root_dir=/home/as26840@ens.ad.etsmtl.ca/data/wildfirespreadTS_hdf5 --data.predict_range=24 --data.batch_size=2
+python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.devices=1
 
 # SeasFire
 python src/SeasFire/train.py --config configs/SeasFire.yaml --trainer.devices=1
