f878a95000700c9da8083d44a2e57f0d01cdf742
aamer98 <ar.aamer@gmail.com>
2024-08-12 13:53:21 -0400

    scripts

--- train_scripts.sh
+++ train_scripts.sh
@@ -1,5 +1,5 @@
 # wildfire unit test
-python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.devices=1 --data.root_dir=/home/as26840@ens.ad.etsmtl.ca/data/wildfirespreadTS_unitTest 
+python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.devices=1 --data.root_dir=/home/aamer98/projects/def-ebrahimi/aamer98/data/wildfirespreadTS_unitTest
 
 # wildfire
 CUDA_VISIBLE_DEVICES=1 python src/WildfireSpreadTS/train.py --config configs/WildfireSpreadTS.yaml --trainer.devices=1 --model.pretrained_res=5.625deg
